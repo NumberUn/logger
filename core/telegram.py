07d887328c22a8fe25e791ad5d4536dbdf85eab7
HFTlab <arbitragecryptobotnet@gmail.com>
2024-01-18 18:28:01 +0100

    cleaned telegram from extra groups and tokens

--- core/telegram.py
+++ core/telegram.py
@@ -12,16 +12,10 @@ config.read('config.ini', "utf-8")
 class TG_Groups(Enum):
     _main_id = int(config['TELEGRAM']['CHAT_ID'])
     _main_token = config['TELEGRAM']['TOKEN']
-    # self.daily_chat_id = int(config['TELEGRAM']['DAILY_CHAT_ID'])
-    # self.inv_chat_id = int(config['TELEGRAM']['INV_CHAT_ID'])
     _alert_id = int(config['TELEGRAM']['ALERT_CHAT_ID'])
-    _alert_token = config['TELEGRAM']['ALERT_BOT_TOKEN']
-    _debug_id = int(config['TELEGRAM']['DIMA_DEBUG_CHAT_ID'])
-    _debug_token = config['TELEGRAM']['DIMA_DEBUG_BOT_TOKEN']
 
     MainGroup = {'chat_id': _main_id, 'bot_token': _main_token}
-    Alerts = {'chat_id': _alert_id, 'bot_token': _alert_token}
-    DebugDima = {'chat_id': _debug_id, 'bot_token': _debug_token}
+    Alerts = {'chat_id': _alert_id, 'bot_token': _main_token}
 
 
 class Telegram:
