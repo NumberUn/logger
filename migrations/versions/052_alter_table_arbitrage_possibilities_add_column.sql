e41b1990192038226862116cbc4df679c011d1c3
HFTlab <arbitragecryptobotnet@gmail.com>
2024-03-01 15:26:15 +0100

    added new column target profit

--- migrations/versions/052_alter_table_arbitrage_possibilities_add_column.sql
+++ migrations/versions/052_alter_table_arbitrage_possibilities_add_column.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 LOCK arbitrage_possibilities IN ROW EXCLUSIVE MODE NOWAIT;
 
-ALTER TABLE arbitrage_possibilities add column env varchar(64);
+ALTER TABLE arbitrage_possibilities add column target_profit FLOAT;
 
 COMMIT;
